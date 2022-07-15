bb1e643502fdad5a39bbd553572e0f3d8f4abe49
Henry Lukito <30253226+henrylukito@users.noreply.github.com>
2022-07-15 13:36:06 +0800

    Update db.py
    Re-implement propkeyvalue

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import os, yaml
 def load(schemapath):
   global n; n = {}
   global s; s = {}
+  global p; p = {}
   with open(schemapath, encoding='utf-8') as schemafile:
     schemadict = yaml.safe_load(schemafile)
   schemadirname = os.path.dirname(schemapath)
@@ -12,23 +13,23 @@ def load(schemapath):
 
 def assertid(id, newnodestr):
   if newnodestr == 'all' and id in n:
-    raise AssertionError(f"{id} is expected to be new, but already exist")
+    raise AssertionError(f"{id} expected new, but already exist")
   elif newnodestr == 'none' and id not in n:
-    raise AssertionError(f"{id} is expected to already exist, but not")
+    raise AssertionError(f"{id} expected already exist, but new")
 
-def setsfromstr(setstr):
-  return [token.strip() for token in setstr.split(",") if token.strip()]
-
-def addidstosets(ids, sets):
-  for setid in sets:
-    s.setdefault(setid, {}).update(dict.fromkeys(ids))
+def handleids(ids, newnodestr, setstr):
+  if len(ids) == 0: return
+  sets = [token.strip() for token in setstr.split(",") if token.strip()]
+  for id in ids: assertid(id, newnodestr); n.setdefault(id)
+  for setid in sets: s.setdefault(setid, {}).update(dict.fromkeys(ids))
 
 def idlist(info):
   with open(info['path'], encoding='utf-8') as file:
     ids = [id.strip() for id in file.readlines() if id.strip()]
-  newnodestr = info.get('new-node', 'all')
-  sets = setsfromstr(info.get('sets', ''))
-  for id in ids:
-    assertid(id, newnodestr)
-    n.setdefault(id)
-  addidstosets(ids, sets)
+  handleids(ids, info.get('new-node', 'all'), info.get('sets', ''))
+
+def propkeyvalue(info):
+  with open(info['path'], encoding='utf-8') as file:
+    filedict = yaml.safe_load(file)
+  handleids(list(filedict), info.get('new-node', 'none'), info.get('sets', ''))
+  p.setdefault(info['propname'], {}).update(filedict)
